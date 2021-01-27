9a1a5362d0ccfffe80d138caab09d684a9ebc4a7
dkje <developer.kje@gmail.com>
2021-01-27 10:42:48 +0900

    useResize hook added for svg

---
+++ src/common/hooks/useResizeObserver.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import ResizeObserver from "resize-observer-polyfill";
+
+const useResizeObserver = (ref: any) => {
+  const [dimensions, setDimensions] = useState<any>(null);
+
+  useEffect(() => {
+    const observeTarget = ref.current;
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        setDimensions(entry.contentRect);
+      }
+    });
+
+    resizeObserver.observe(observeTarget);
+
+    return () => {
+      resizeObserver.unobserve(observeTarget);
+    };
+  }, [ref]);
+
+  return dimensions;
+};
+
+export default useResizeObserver;
